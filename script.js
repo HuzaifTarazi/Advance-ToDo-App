3e41f4cd8306f65855c00d31095e18dc5442d7e9
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-03-31 19:04:00 +0500

    HTML, CSS, JS: Styling , structuring and boiler plate dne

---
+++ script.js
@@ -0,0 +1,8 @@
+const taskInput = document.getElementById("task-input")
+const saveBtn = document.getElementById("save-btn")
+
+saveBtn.onclick = () =>{
+    const userInput = taskInput.value
+
+    console.log(userInput)
+}
